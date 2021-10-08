edd776d9c18e99378fad24c3cfb7ea5a74eb1f05
Xhunik Miguel <xhuniktzi@gmail.com>
2021-10-08 08:57:05 -0600

    errors in views 3

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NotifyService } from 'src/app/shared/notify.service';
 import { IProduct } from '../../common/product';
 import { ProductsApiService } from '../../data/products-api.service';
 
@@ -13,13 +14,20 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private productsApiService: ProductsApiService
+    private productsApiService: ProductsApiService,
+    private notifyService: NotifyService
   ) {}
 
   ngOnInit(): void {
     const code = this.route.snapshot.paramMap.get('code');
     this.productsApiService.findProductByCode(code).subscribe({
       next: (product) => (this.product = product),
+      error: () => {
+        this.notifyService.show({
+          type: 'error',
+          msg: 'El producto no existe',
+        });
+      },
     });
   }
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifyService } from 'src/app/shared/notify.service';
 import { IProduct } from '../../common/product';
 import { ProductsApiService } from '../../data/products-api.service';
 
@@ -8,12 +9,21 @@ import { ProductsApiService } from '../../data/products-api.service';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  constructor(private productsApiService: ProductsApiService) {}
+  constructor(
+    private productsApiService: ProductsApiService,
+    private notifyService: NotifyService
+  ) {}
   products: IProduct[] = [];
 
   ngOnInit(): void {
     this.productsApiService.getProducts().subscribe({
       next: (products) => (this.products = products),
+      error: () => {
+        this.notifyService.show({
+          type: 'error',
+          msg: 'Error al cargar la lista de productos',
+        });
+      },
     });
   }
 }
