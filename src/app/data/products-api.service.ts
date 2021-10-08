6e52deabaaa047635d1ac0097e384861085fcc78
Xhunik Miguel <xhuniktzi@gmail.com>
2021-10-08 13:31:31 -0600

    add env variables

--- src/app/data/products-api.service.ts
+++ src/app/data/products-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { IProduct } from '../common/product';
 
 @Injectable({
@@ -13,35 +14,35 @@ export class ProductsApiService {
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
-    const url = 'http://localhost:5000/products';
+    const url = `${environment.api}/products`;
     return this.http
       .get<IProduct[]>(url)
       .pipe((data) => data, catchError(this.handleError));
   }
 
   findProductByCode(code: string | null): Observable<IProduct> {
-    const url = `http://localhost:5000/products/${code}`;
+    const url = `${environment.api}/products/${code}`;
     return this.http
       .get<IProduct>(url)
       .pipe((data) => data, catchError(this.handleError));
   }
 
   saveProduct(product: IProduct): Observable<any> {
-    const url = 'http://localhost:5000/products';
+    const url = `${environment.api}/products`;
     return this.http
       .post(url, product)
       .pipe((data) => data, catchError(this.handleError));
   }
 
   updateProduct(code: string, product: IProduct): Observable<any> {
-    const url = `http://localhost:5000/products/${code}`;
+    const url = `${environment.api}/products/${code}`;
     return this.http
       .put(url, product)
       .pipe((data) => data, catchError(this.handleError));
   }
 
   deleteProduct(code: string | undefined): Observable<any> {
-    const url = `http://localhost:5000/products/${code}`;
+    const url = `${environment.api}/products/${code}`;
     return this.http
       .delete(url)
       .pipe((data) => data, catchError(this.handleError));
